4bd0d98af29c4ecbd4fa3016a05c93ddeabf0330
Alexa Drenick <adrenick@calpoly.edu>
2019-04-28 23:07:00 -0700

    abstracted clang and a.out unit testing

--- src/test/java/Benchmarks.java
+++ src/test/java/Benchmarks.java
@@ -1,448 +1,428 @@
 import mini.MiniCompiler;
 import org.junit.*;
 
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
+
+import static junit.framework.TestCase.fail;
+
+
 public class Benchmarks {
 
     //@Rule
     //TODO
     //public MiniCompiler mini = new MiniCompiler();
 
-    @Test
-    public void BenchMarkishTopics()
+    String readHelper = "../../../../main/resources/read_util.c";
+
+    void runClang(String wd, String file)
     {
-        MiniCompiler mini = new MiniCompiler();
-        String file = "benchmarks/BenchMarkishTopics/BenchMarkishTopics.mini";
-        String[] args = {file};
-        mini.main(args);
-        /*
+        ProcessBuilder clang = new ProcessBuilder("clang", file, readHelper);
+        File dir = new File(wd).getAbsoluteFile();
+        clang.redirectErrorStream(true);
+        clang.directory(dir);
         try
         {
-            Process c = Runtime.getRuntime().exec("clang benchmarks/BenchMarkishTopics/BenchMarkishTopics.ll");
-            Process p = Runtime.getRuntime().exec("/a.out");
+            Process p = clang.start();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+            String line;
+            while ((line = reader.readLine()) != null)
+            {
+                System.out.println(line);
+            }
+
         }
         catch(Exception e)
         {
             System.err.println("exception: " + e.getMessage());
             System.exit(-2);
         }
-        */
+    }
+
+    boolean runAout(String wd)
+    {
+        ProcessBuilder aout = new ProcessBuilder("./a.out");
+        File dir = new File(wd).getAbsoluteFile();
+        aout.redirectErrorStream(true);
+        aout.directory(dir);
+        try
+        {
+            Process p = aout.start();
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(p.getOutputStream()));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            writer.write("5\n");
+
+            // TODO figure out how to read output
+            /*
+            String line;
+            while ((line = reader.readLine()) != null)
+            {
+                System.out.println(line);
+            }
+            */
+        }
+        catch(Exception e)
+        {
+            System.err.println("exception: " + e.getMessage());
+            return false;
+        }
+        return true;
+    }
+
+    @Test
+    public void BenchMarkishTopics()
+    {
+        MiniCompiler mini = new MiniCompiler();
+        String dir = "benchmarks/BenchMarkishTopics";
+        String file = "BenchMarkishTopics";
+        String[] args = {dir + "/" + file + ".mini"};
+        mini.main(args);
+
+        runClang(dir, file + ".ll");
+
+        if (!(runAout(dir)))
+        {
+            fail("a.out");
+        }
+
+        // TODO null
     }
 
     @Test
     public void bert()
     {
         MiniCompiler mini = new MiniCompiler();
-        String file = "benchmarks/bert/bert.mini";
-        String[] args = {file};
+        String dir = "benchmarks/bert";
+        String file = "bert";
+        String[] args = {dir + "/" + file + ".mini"};
         mini.main(args);
         // TODO null
-        /*
-        try
+
+        runClang(dir, file + ".ll");
+
+        if (!(runAout(dir)))
         {
-            Process c = Runtime.getRuntime().exec("clang benchmarks/bert/bert.ll");
-            Process p = Runtime.getRuntime().exec("/a.out");
+            fail("a.out");
         }
-        catch(Exception e)
-        {
-            System.err.println("exception: " + e.getMessage());
-            System.exit(-2);
-        }
-        */
     }
 
     @Test
     public void biggest()
     {
         MiniCompiler mini = new MiniCompiler();
-        String file = "benchmarks/biggest/biggest.mini";
-        String[] args = {file};
+        String dir = "benchmarks/biggest";
+        String file = "biggest";
+        String[] args = {dir + "/" + file + ".mini"};
         mini.main(args);
-        // TODO read
-        /*
-        try
+        // TODO getelementptr %
+
+        runClang(dir, file + ".ll");
+
+        if (!(runAout(dir)))
         {
-            Process c = Runtime.getRuntime().exec("clang benchmarks/biggest/biggest.ll");
-            Process p = Runtime.getRuntime().exec("/a.out");
+            fail("a.out");
         }
-        catch(Exception e)
-        {
-            System.err.println("exception: " + e.getMessage());
-            System.exit(-2);
-        }
-        */
     }
 
     @Test
     public void binaryConverter()
     {
         MiniCompiler mini = new MiniCompiler();
-        String file = "benchmarks/binaryConverter/binaryConverter.mini";
-        String[] args = {file};
+        String dir = "benchmarks/binaryConverter";
+        String file = "binaryConverter";
+        String[] args = {dir + "/" + file + ".mini"};
         mini.main(args);
-        // TODO read
-        /*
-        try
+        // TODO a.out doesn't output anything
+
+        runClang(dir, file + ".ll");
+
+        if (!(runAout(dir)))
         {
-            Process c = Runtime.getRuntime().exec("clang benchmarks/binaryConverter/binaryConverter.ll");
-            Process p = Runtime.getRuntime().exec("/a.out");
+            fail("a.out");
         }
-        catch(Exception e)
-        {
-            System.err.println("exception: " + e.getMessage());
-            System.exit(-2);
-        }
-        */
     }
 
     @Test
     public void brett()
     {
         MiniCompiler mini = new MiniCompiler();
-        String file = "benchmarks/brett/brett.mini";
-        String[] args = {file};
+        String dir = "benchmarks/brett";
+        String file = "brett";
+        String[] args = {dir + "/" + file + ".mini"};
         mini.main(args);
         // TODO true
-        /*
-        try
+
+        runClang(dir, file + ".ll");
+
+        if (!(runAout(dir)))
         {
-            Process c = Runtime.getRuntime().exec("clang benchmarks/brett/brett.ll");
-            Process p = Runtime.getRuntime().exec("/a.out");
+            fail("a.out");
         }
-        catch(Exception e)
-        {
-            System.err.println("exception: " + e.getMessage());
-            System.exit(-2);
-        }
-        */
+
     }
 
     @Test
     public void creativeBenchMarkName()
     {
         MiniCompiler mini = new MiniCompiler();
-        String file = "benchmarks/creativeBenchMarkName/creativeBenchMarkName.mini";
-        String[] args = {file};
+        String dir = "benchmarks/creativeBenchMarkName";
+        String file = "creativeBenchMarkName";
+        String[] args = {dir + "/" + file + ".mini"};
         mini.main(args);
-        /*
-        try
+
+        runClang(dir, file + ".ll");
+
+        if (!(runAout(dir)))
         {
-            Process c = Runtime.getRuntime().exec("clang benchmarks/creativeBenchMarkName/creativeBenchMarkName.ll");
-            Process p = Runtime.getRuntime().exec("/a.out");
+            fail("a.out");
         }
-        catch(Exception e)
-        {
-            System.err.println("exception: " + e.getMessage());
-            System.exit(-2);
-        }
-        */
+        // TODO getelementptr % maybe
     }
 
     @Test
     public void fact_sum()
     {
         MiniCompiler mini = new MiniCompiler();
-        String file = "benchmarks/fact_sum/fact_sum.mini";
-        String[] args = {file};
+        String file = "fact_sum";
+        String dir = "benchmarks/fact_sum";
+        String[] args = {dir + "/" + file + ".mini"};
         mini.main(args);
-        // TODO bool size probs
-        /*
-        try
+
+        runClang(dir, file + ".ll");
+
+        if (!(runAout(dir)))
         {
-            Process c = Runtime.getRuntime().exec("clang benchmarks/fact_sum/fact_sum.ll");
-            Process p = Runtime.getRuntime().exec("/a.out");
+            fail("a.out");
         }
-        catch(Exception e)
-        {
-            System.err.println("exception: " + e.getMessage());
-            System.exit(-2);
-        }
-        */
+        // TODO some incorrect output
     }
 
     @Test
     public void Fibonacci()
     {
         MiniCompiler mini = new MiniCompiler();
-        String file = "benchmarks/Fibonacci/Fibonacci.mini";
-        String[] args = {file};
+        String dir = "benchmarks/Fibonacci";
+        String file = "Fibonacci";
+        String[] args = {dir + "/" + file + ".mini"};
         mini.main(args);
+
         // TODO cond exit blocks
-        /*
-        try
+        runClang(dir, file + ".ll");
+
+        if (!(runAout(dir)))
         {
-            Process c = Runtime.getRuntime().exec("clang benchmarks/Fibonacci/Fibonacci.ll");
-            Process p = Runtime.getRuntime().exec("/a.out");
+            fail("a.out");
         }
-        catch(Exception e)
-        {
-            System.err.println("exception: " + e.getMessage());
-            System.exit(-2);
-        }
-        */
     }
 
     @Test
     public void GeneralFunctAndOptimize()
     {
         MiniCompiler mini = new MiniCompiler();
-        String file = "benchmarks/GeneralFunctAndOptimize/GeneralFunctAndOptimize.mini";
-        String[] args = {file};
+        String dir = "benchmarks/GeneralFunctAndOptimize";
+        String file = "GeneralFunctAndOptimize";
+        String[] args = {dir + "/" + file + ".mini"};
         mini.main(args);
+
         // TODO no percent on getelementptr when Local Value
-        /*
-        try
+        runClang(dir, file + ".ll");
+
+        if (!(runAout(dir)))
         {
-            Process c = Runtime.getRuntime().exec("clang benchmarks/GeneralFunctAndOptimize/GeneralFunctAndOptimize.ll");
-            Process p = Runtime.getRuntime().exec("/a.out");
+            fail("a.out");
         }
-        catch(Exception e)
-        {
-            System.err.println("exception: " + e.getMessage());
-            System.exit(-2);
-        }
-        */
     }
 
     @Test
     public void hailstone()
     {
         MiniCompiler mini = new MiniCompiler();
-        String file = "benchmarks/hailstone/hailstone.mini";
-        String[] args = {file};
+        String dir = "benchmarks/hailstone";
+        String file = "hailstone";
+        String[] args = {dir + "/" + file + ".mini"};
         mini.main(args);
-        // TODO undefined label
-        /*
-        try
+        // TODO true
+        runClang(dir, file + ".ll");
+
+        if (!(runAout(dir)))
         {
-            Process c = Runtime.getRuntime().exec("clang benchmarks/hailstone/hailstone.ll");
-            Process p = Runtime.getRuntime().exec("/a.out");
+            fail("a.out");
         }
-        catch(Exception e)
-        {
-            System.err.println("exception: " + e.getMessage());
-            System.exit(-2);
-        }
-        */
     }
 
     @Test
     public void hanoi_benchmark()
     {
         MiniCompiler mini = new MiniCompiler();
-        String file = "benchmarks/hanoi_benchmark/hanoi_benchmark.mini";
-        String[] args = {file};
+        String dir = "benchmarks/hanoi_benchmark";
+        String file = "hanoi_benchmark";
+        String[] args = {dir + "/" + file + ".mini"};
         mini.main(args);
         // TODO same getelementptr bug
-        /*
-        try
+        runClang(dir, file + ".ll");
+
+        if (!(runAout(dir)))
         {
-            Process c = Runtime.getRuntime().exec("clang benchmarks/hanoi_benchmark/hanoi_benchmark.ll");
-            Process p = Runtime.getRuntime().exec("/a.out");
+            fail("a.out");
         }
-        catch(Exception e)
-        {
-            System.err.println("exception: " + e.getMessage());
-            System.exit(-2);
-        }
-        */
     }
 
     @Test
     public void killerBubbles()
     {
         MiniCompiler mini = new MiniCompiler();
-        String file = "benchmarks/killerBubbles/killerBubbles.mini";
-        String[] args = {file};
+        String dir = "benchmarks/killerBubbles";
+        String file = "killerBubbles";
+        String[] args = {dir + "/" + file + ".mini"};
         mini.main(args);
         // TODO same getelementptr bug
-        /*
-        try
+        runClang(dir, file + ".ll");
+
+        if (!(runAout(dir)))
         {
-            Process c = Runtime.getRuntime().exec("clang benchmarks/killerBubbles/killerBubbles.ll");
-            Process p = Runtime.getRuntime().exec("/a.out");
+            fail("a.out");
         }
-        catch(Exception e)
-        {
-            System.err.println("exception: " + e.getMessage());
-            System.exit(-2);
-        }
-        */
     }
 
     @Test
     public void mile1()
     {
         MiniCompiler mini = new MiniCompiler();
-        String file = "benchmarks/mile1/mile1.mini";
-        String[] args = {file};
+        String dir = "benchmarks/mile1";
+        String file = "mile1";
+        String[] args = {dir + "/" + file + ".mini"};
         mini.main(args);
-        // TODO same read bug
-        /*
-        try
+        // TODO getelementptr bug
+        runClang(dir, file + ".ll");
+
+        if (!(runAout(dir)))
         {
-            Process c = Runtime.getRuntime().exec("clang mile1.ll");
-            Process p = Runtime.getRuntime().exec("/a.out");
+            fail("a.out");
         }
-        catch(Exception e)
-        {
-            System.err.println("exception: " + e.getMessage());
-            System.exit(-2);
-        }
-        */
     }
 
     @Test
     public void mixed()
     {
         MiniCompiler mini = new MiniCompiler();
-        String file = "benchmarks/mixed/mixed.mini";
-        String[] args = {file};
+        String dir = "benchmarks/mixed";
+        String file = "mixed";
+        String[] args = {dir + "/" + file + ".mini"};
         mini.main(args);
         // TODO empty block
-        /*
-        try
+        runClang(dir, file + ".ll");
+
+        if (!(runAout(dir)))
         {
-            Process c = Runtime.getRuntime().exec("clang benchmarks/mixed/mixed.ll");
-            Process p = Runtime.getRuntime().exec("/a.out");
+            fail("a.out");
         }
-        catch(Exception e)
-        {
-            System.err.println("exception: " + e.getMessage());
-            System.exit(-2);
-        }
-        */
     }
 
     @Test
     public void OptimizationBenchmark()
     {
         MiniCompiler mini = new MiniCompiler();
-        String file = "benchmarks/OptimizationBenchmark/OptimizationBenchmark.mini";
-        String[] args = {file};
+        String dir = "benchmarks/OptimizationBenchmark";
+        String file = "OptimizationBenchmark";
+        String[] args = {dir + "/" + file + ".mini"};
         mini.main(args);
-        // TODO i1 type
-        /*
-        try
+
+        runClang(dir, file + ".ll");
+
+        if (!(runAout(dir)))
         {
-            Process c = Runtime.getRuntime().exec("clang benchmarks/OptimizationBenchmark/OptimizationBenchmark.ll");
-            Process p = Runtime.getRuntime().exec("/a.out");
+            fail("a.out");
         }
-        catch(Exception e)
-        {
-            System.err.println("exception: " + e.getMessage());
-            System.exit(-2);
-        }
-        */
+        // TODO some incorrect output
     }
 
     @Test
     public void primes()
     {
         MiniCompiler mini = new MiniCompiler();
-        String file = "benchmarks/primes/primes.mini";
-        String[] args = {file};
+        String dir = "benchmarks/primes";
+        String file = "primes";
+        String[] args = {dir + "/" + file + ".mini"};
         mini.main(args);
         // TODO false
-        /*
-        try
+        runClang(dir, file + ".ll");
+
+        if (!(runAout(dir)))
         {
-            Process c = Runtime.getRuntime().exec("clang benchmarks/primes/primes.ll");
-            Process p = Runtime.getRuntime().exec("/a.out");
+            fail("a.out");
         }
-        catch(Exception e)
-        {
-            System.err.println("exception: " + e.getMessage());
-            System.exit(-2);
-        }
-        */
     }
 
     @Test
     public void programBreaker()
     {
         MiniCompiler mini = new MiniCompiler();
-        String file = "benchmarks/programBreaker/programBreaker.mini";
-        String[] args = {file};
+        String dir = "benchmarks/programBreaker";
+        String file = "programBreaker";
+        String[] args = {dir + "/" + file + ".mini"};
         mini.main(args);
-        // TODO i1 issue
-        /*
-        try
+
+        runClang(dir, file + ".ll");
+
+        if (!(runAout(dir)))
         {
-            Process c = Runtime.getRuntime().exec("clang benchmarks/programBreaker/programBreaker.ll");
-            Process p = Runtime.getRuntime().exec("/a.out");
+            fail("a.out");
         }
-        catch(Exception e)
-        {
-            System.err.println("exception: " + e.getMessage());
-            System.exit(-2);
-        }
-        */
     }
 
     @Test
     public void stats()
     {
         MiniCompiler mini = new MiniCompiler();
-        String file = "benchmarks/stats/stats.mini";
-        String[] args = {file};
+        String dir = "benchmarks/stats";
+        String file = "stats";
+        String[] args = {dir + "/" + file + ".mini"};
         mini.main(args);
         // TODO same getelementptr bug
-        /*
-        try
+        runClang(dir, file + ".ll");
+
+        if (!(runAout(dir)))
         {
-            Process c = Runtime.getRuntime().exec("clang benchmarks/stats/stats.ll");
-            Process p = Runtime.getRuntime().exec("/a.out");
+            fail("a.out");
         }
-        catch(Exception e)
-        {
-            System.err.println("exception: " + e.getMessage());
-            System.exit(-2);
-        }
-        */
     }
 
     @Test
     public void TicTac()
     {
         MiniCompiler mini = new MiniCompiler();
-        String file = "benchmarks/TicTac/TicTac.mini";
-        String[] args = {file};
+        String dir = "benchmarks/TicTac";
+        String file = "TicTac";
+        String[] args = {dir + "/" + file + ".mini"};
         mini.main(args);
         // TODO getelementptr issue
-        /*
-        try
+        runClang(dir, file + ".ll");
+
+        if (!(runAout(dir)))
         {
-            Process c = Runtime.getRuntime().exec("clang benchmarks/TicTac/TicTac.ll");
-            Process p = Runtime.getRuntime().exec("/a.out");
+            fail("a.out");
         }
-        catch(Exception e)
-        {
-            System.err.println("exception: " + e.getMessage());
-            System.exit(-2);
-        }
-        */
     }
 
     @Test
     public void wasteOfCycles()
     {
         MiniCompiler mini = new MiniCompiler();
-        String file = "benchmarks/wasteOfCycles/wasteOfCycles.mini";
-        String[] args = {file};
-        // TODO read
-        /*
+        String dir = "benchmarks/wasteOfCycles";
+        String file = "wasteOfCycles";
+        String[] args = {dir + "/" + file + ".mini"};
         mini.main(args);
-        try
+        // TODO some incorrect input
+        runClang(dir, file + ".ll");
+
+        if (!(runAout(dir)))
         {
-            Process c = Runtime.getRuntime().exec("clang benchmarks/wasteOfCycles/wasteOfCycles.ll");
-            Process p = Runtime.getRuntime().exec("/a.out");
+            fail("a.out");
         }
-        catch(Exception e)
-        {
-            System.err.println("exception: " + e.getMessage());
-            System.exit(-2);
-        }
-        */
     }
 
 }
