123f581858e465b896e474b2ee2d4ca7c62370e7
JasonLomsdalen <jlomsdal@calpoly.edu>
2019-06-12 21:06:26 -0700

    Updated Scripts

--- runCompilerStats.sh
+++ runCompilerStats.sh
@@ -2,7 +2,7 @@
 
 printf "Running benchmarks.....\n\n"
 
-cd benchmarks
+cd benchmarksSSCP
 
 printf "Running LLVM SSA Benchmark Tests.....\n"
 for file in * ; 
@@ -13,46 +13,12 @@ do
 	then 
 		printf "$file:\n\n"
 
-		java -cp ${CLASSPATH}:/Users/alexadrenick/.m2/repository/org/antlr/antlr4-runtime/4.7.1/antlr4-runtime-4.7.1.jar:/Users/alexadrenick/Documents/*Poly/*Spring19/CSC431/MiniCompiler/target/classes mini.MiniCompiler $file/$file.mini -llvm
+		java -cp ${CLASSPATH}:/Users/JasonComputer/.m2/repository/org/antlr/antlr4-runtime/4.7.1/antlr4-runtime-4.7.1.jar:/Users/JasonComputer/Documents/CompSciStuff/cpe431/MiniCompiler/target/classes mini.MiniCompiler $file/$file.mini -llvm -uce -lvn -sscp
 		if [ -e $file/$file.ll ];
 		then 
 			printf "word count for ssa: "
 			wc -l $file/$file.ll
 			printf "\n\n"
-			clang $file/$file.ll ../src/main/resources/read_util.c
-			if [ -e a.out ];
-			then 
-				chmod 777 $file/input.longer
-				time ./a.out < $file/input.longer > /dev/null 2>&1
-			fi
-			[ -e $file/$file.ll ] && rm $file/$file.ll
-			[ -e $file/$file.s ] && rm $file/$file.s
-		fi
-		printf "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n"
-	fi
-done
-
-printf "Running LLVM SSA Benchmark Tests with Optimizations.....\n"
-for file in * ; 
-do 
-	[ -e a.out ] && rm a.out
-
-	if [ $file != "a.out" ];
-	then 
-		printf "$file:\n\n"
-
-		java -cp ${CLASSPATH}:/Users/alexadrenick/.m2/repository/org/antlr/antlr4-runtime/4.7.1/antlr4-runtime-4.7.1.jar:/Users/alexadrenick/Documents/*Poly/*Spring19/CSC431/MiniCompiler/target/classes mini.MiniCompiler $file/$file.mini -uce -llvm -lvn
-		if [ -e $file/$file.ll ];
-		then 
-
-			clang $file/$file.ll ../src/main/resources/read_util.c
-			if [ -e a.out ];
-			then 
-				chmod 777 $file/input.longer
-				time ./a.out < $file/input.longer > /dev/null 2>&1
-			fi
-			[ -e $file/$file.ll ] && rm $file/$file.ll
-			[ -e $file/$file.s ] && rm $file/$file.s
 		fi
 		printf "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n"
 	fi
--- runStats.sh
+++ runStats.sh
@@ -2,20 +2,21 @@
 
 printf "Running benchmarks.....\n\n"
 
-cd benchmarks
+cd inputFiles
 
 printf "Running Clang with No Optimizations.....\n"
-for file in * ; 
-do 
+for file in * ;
+do
 	[ -e a.out ] && rm a.out
 
 	if [ $file != "a.out" ];
-	then 
+	then
 		printf "clang no-optimization time for $file:\n"
-		gcc -w -S -O0 -emit-llvm $file/$file.c
+		# gcc -w -S -O0 -emit-llvm $file/$file.c
+		clang $file/$file.c -w -S -O0 -emit-llvm
 		clang $file.ll
 		if [ -e a.out ];
-		then 
+		then
 			chmod 777 $file/input.longer
 			time ./a.out < $file/input.longer > /dev/null 2>&1
 		fi
@@ -26,17 +27,18 @@ do
 done
 
 printf "Running Clang with Optimizations.....\n"
-for file in * ; 
-do 
+for file in * ;
+do
 	[ -e a.out ] && rm a.out
 
 	if [ $file != "a.out" ];
-	then 
+	then
 		printf "clang optimization time for $file:\n"
-		gcc -w -S -O3 -emit-llvm $file/$file.c
+		# gcc -w -S -O3 -emit-llvm $file/$file.c
+		clang $file/$file.c -w -S -O3 -emit-llvm
 		clang $file.ll
 		if [ -e a.out ];
-		then 
+		then
 			chmod 777 $file/input.longer
 			time ./a.out < $file/input.longer > /dev/null 2>&1
 		fi
@@ -46,82 +48,105 @@ do
 	fi
 done
 
-printf "Running LLVM Stack-Based Benchmarks.....\n"
-for file in * ; 
-do 
+printf "Running register-based-llvm-no-op .....\n"
+for file in * ;
+do
 	[ -e a.out ] && rm a.out
 
 	if [ $file != "a.out" ];
-	then 
+	then
 		printf "$file test:\n\n"
 
-		java -cp ${CLASSPATH}:/Users/alexadrenick/.m2/repository/org/antlr/antlr4-runtime/4.7.1/antlr4-runtime-4.7.1.jar:/Users/alexadrenick/Documents/*Poly/*Spring19/CSC431/MiniCompiler/target/classes mini.MiniCompiler $file/$file.mini -llvm -stack
-		if [ -e $file.ll ];
-		then 
-			printf "word count for stack-based: "
-			wc -l $file.ll
-			printf "\n\n"
-			clang $file.ll ../src/main/java/mini/read_helper.c
+		# java -cp ${CLASSPATH}:/Users/alexadrenick/.m2/repository/org/antlr/antlr4-runtime/4.7.1/antlr4-runtime-4.7.1.jar:/Users/alexadrenick/Documents/*Poly/*Spring19/CSC431/MiniCompiler/target/classes mini.MiniCompiler -uce -llvm -lvn
+		if [ -e "../register-based-llvm-no-op/$file.ll" ];
+		then
+
+			clang "../register-based-llvm-no-op/$file.ll" read_util.c
 			if [ -e a.out ];
-			then 
+			then
 				chmod 777 $file/input.longer
 				time ./a.out < $file/input.longer > /dev/null 2>&1
 			fi
-			[ -e $file.ll ] && rm $file.ll
-			[ -e $file.s ] && rm $file.s
-		fi
-		printf "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n"
-	fi
-done
-printf "Running LLVM SSA Benchmark Tests.....\n"
-for file in * ; 
-do 
-	[ -e a.out ] && rm a.out
-
-	if [ $file != "a.out" ];
-	then 
-		printf "$file test:\n\n"
-
-		java -cp ${CLASSPATH}:/Users/alexadrenick/.m2/repository/org/antlr/antlr4-runtime/4.7.1/antlr4-runtime-4.7.1.jar:/Users/alexadrenick/Documents/*Poly/*Spring19/CSC431/MiniCompiler/target/classes mini.MiniCompiler -llvm
-		if [ -e $file.ll ];
-		then 
-			printf "word count for ssa: "
-			wc -l $file.ll
-			printf "\n\n"
-			clang $file.ll ../src/main/java/mini/read_helper.c
-			if [ -e a.out ];
-			then 
-				chmod 777 $file/input.longer
-				time ./a.out < $file/input.longer > /dev/null 2>&1
-			fi
-			[ -e $file.ll ] && rm $file.ll
-			[ -e $file.s ] && rm $file.s
+			# [ -e $file.ll ] && rm $file.ll
+			# [ -e $file.s ] && rm $file.s
 		fi
 		printf "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n"
 	fi
 done
 
-printf "Running LLVM SSA Benchmark Tests with Optimizations.....\n"
-for file in * ; 
-do 
+printf "Running register-based-llvm-with-sscp-op .....\n"
+for file in * ;
+do
 	[ -e a.out ] && rm a.out
 
 	if [ $file != "a.out" ];
-	then 
+	then
 		printf "$file test:\n\n"
 
-		java -cp ${CLASSPATH}:/Users/alexadrenick/.m2/repository/org/antlr/antlr4-runtime/4.7.1/antlr4-runtime-4.7.1.jar:/Users/alexadrenick/Documents/*Poly/*Spring19/CSC431/MiniCompiler/target/classes mini.MiniCompiler -uce -llvm -lvn
-		if [ -e $file.ll ];
-		then 
+		# java -cp ${CLASSPATH}:/Users/alexadrenick/.m2/repository/org/antlr/antlr4-runtime/4.7.1/antlr4-runtime-4.7.1.jar:/Users/alexadrenick/Documents/*Poly/*Spring19/CSC431/MiniCompiler/target/classes mini.MiniCompiler -uce -llvm -lvn
+		if [ -e "../register-based-llvm-w-sscp-op/$file.ll" ];
+		then
 
-			clang $file.ll ../src/main/java/mini/read_helper.c
+			clang "../register-based-llvm-w-sscp-op/$file.ll" read_util.c
 			if [ -e a.out ];
-			then 
+			then
 				chmod 777 $file/input.longer
 				time ./a.out < $file/input.longer > /dev/null 2>&1
 			fi
-			[ -e $file.ll ] && rm $file.ll
-			[ -e $file.s ] && rm $file.s
+			# [ -e $file.ll ] && rm $file.ll
+			# [ -e $file.s ] && rm $file.s
+		fi
+		printf "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n"
+	fi
+done
+
+printf "Running register-based-llvm-with-op .....\n"
+for file in * ;
+do
+	[ -e a.out ] && rm a.out
+
+	if [ $file != "a.out" ];
+	then
+		printf "$file test:\n\n"
+
+		# java -cp ${CLASSPATH}:/Users/alexadrenick/.m2/repository/org/antlr/antlr4-runtime/4.7.1/antlr4-runtime-4.7.1.jar:/Users/alexadrenick/Documents/*Poly/*Spring19/CSC431/MiniCompiler/target/classes mini.MiniCompiler -uce -llvm -lvn
+		if [ -e "../register-based-llvm-w-op/$file.ll" ];
+		then
+
+			clang "../register-based-llvm-w-op/$file.ll" read_util.c
+			if [ -e a.out ];
+			then
+				chmod 777 $file/input.longer
+				time ./a.out < $file/input.longer > /dev/null 2>&1
+			fi
+			# [ -e $file.ll ] && rm $file.ll
+			# [ -e $file.s ] && rm $file.s
+		fi
+		printf "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n"
+	fi
+done
+
+printf "Running stack-based-llvm-no-op .....\n"
+for file in * ;
+do
+	[ -e a.out ] && rm a.out
+
+	if [ $file != "a.out" ];
+	then
+		printf "$file test:\n\n"
+
+		# java -cp ${CLASSPATH}:/Users/alexadrenick/.m2/repository/org/antlr/antlr4-runtime/4.7.1/antlr4-runtime-4.7.1.jar:/Users/alexadrenick/Documents/*Poly/*Spring19/CSC431/MiniCompiler/target/classes mini.MiniCompiler -uce -llvm -lvn
+		if [ -e "../stack-based-llvm-no-op/$file.ll" ];
+		then
+
+			clang "../stack-based-llvm-no-op/$file.ll" read_util.c
+			if [ -e a.out ];
+			then
+				chmod 777 $file/input.longer
+				time ./a.out < $file/input.longer > /dev/null 2>&1
+			fi
+			# [ -e $file.ll ] && rm $file.ll
+			# [ -e $file.s ] && rm $file.s
 		fi
 		printf "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n"
 	fi
